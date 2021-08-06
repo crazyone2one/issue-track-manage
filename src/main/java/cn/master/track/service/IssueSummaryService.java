0daa9ddeb56455fde74c62f60196065c92edd2d1
jingll.li <the2ndindec@hotmail.com>
2021-08-06 17:33:57 +0800

    :zap: searchSummaryPage

--- src/main/java/cn/master/track/service/IssueSummaryService.java
+++ src/main/java/cn/master/track/service/IssueSummaryService.java
@@ -2,22 +2,59 @@ package cn.master.track.service;
 
 import cn.master.track.entity.IssueItem;
 import cn.master.track.entity.IssueSummary;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import java.util.Map;
+
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 11's papa
  * @since 2021-08-04
  */
 public interface IssueSummaryService extends IService<IssueSummary> {
-
+    /**
+     * 添加问题单时自动创建任务汇总数据
+     *
+     * @param item 问题单
+     */
     void addIssueSummary(IssueItem item);
 
-    IssueSummary findIssueById(String id);
+    /**
+     * 通过问题单的主键id查询任务汇总数据
+     *
+     * @param id 问题单主键id
+     * @return cn.master.track.entity.IssueSummary
+     */
+    IssueSummary findIssueByProjectId(String id);
+
+    /**
+     * 通过主键id查询任务汇总数据
+     *
+     * @param id 任务汇总主键id
+     * @return cn.master.track.entity.IssueSummary
+     */
+    IssueSummary findSummaryById(String id);
 
     Page<IssueSummary> pageSummary(Integer pageIndex, Integer pageCount);
+
+    /**
+     * 查询任务汇总数据
+     *
+     * @param page
+     * @param wrapper
+     * @return com.baomidou.mybatisplus.extension.plugins.pagination.Page<cn.master.track.entity.IssueSummary>
+     */
+    Page<IssueSummary> searchSummaryPage(Page<IssueSummary> page, QueryWrapper<IssueSummary> wrapper);
+
+    /**
+     * 修改任务汇总数据
+     *
+     * @param params
+     */
+    void modifySummary(Map<String, Object> params);
 }
--- src/main/java/cn/master/track/service/impl/IssueSummaryServiceImpl.java
+++ src/main/java/cn/master/track/service/impl/IssueSummaryServiceImpl.java
@@ -10,6 +10,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Map;
 
 /**
  * <p>
@@ -27,19 +28,56 @@ public class IssueSummaryServiceImpl extends ServiceImpl<IssueSummaryMapper, Iss
         final IssueSummary summary = IssueSummary.builder()
 //                .id(UuidUtils.generate())
                 .projectName(item.getId())
+                .createCaseCount("0")
+                .executeCaseCount("0")
+                .bugDoc("0")
+                .reportDoc("0")
+                .hasDoc("0")
+                .deliveryStatus("0")
                 .createDate(new Date())
                 .build();
         baseMapper.insert(summary);
     }
 
     @Override
-    public IssueSummary findIssueById(String id) {
+    public IssueSummary findIssueByProjectId(String id) {
         return baseMapper.selectOne(new QueryWrapper<IssueSummary>().lambda()
                 .eq(IssueSummary::getProjectName, id));
     }
 
     @Override
+    public IssueSummary findSummaryById(String id) {
+        return baseMapper.selectOne(new QueryWrapper<IssueSummary>().lambda()
+                .eq(IssueSummary::getId, id));
+    }
+
+    @Override
     public Page<IssueSummary> pageSummary(Integer pageIndex, Integer pageCount) {
         return baseMapper.selectPage(new Page<>(pageIndex, pageCount), new QueryWrapper<IssueSummary>().lambda());
+    }
+
+    @Override
+    public Page<IssueSummary> searchSummaryPage(Page<IssueSummary> page, QueryWrapper<IssueSummary> wrapper) {
+        return baseMapper.selectPage(page, wrapper);
+    }
+
+    @Override
+    public void modifySummary(Map<String, Object> params) {
+        final String tempId = params.get("id").toString();
+        final IssueSummary.IssueSummaryBuilder builder = IssueSummary.builder();
+        builder.id(tempId)
+                .projectName(findSummaryById(tempId).getProjectName())
+                .jobDesc(params.get("jobDesc").toString())
+                .createCaseCount(params.get("createCase").toString())
+                .executeCaseCount(params.get("executeCase").toString())
+                .bugDoc(params.get("bugDoc").toString())
+                .reportDoc(params.get("reportDoc").toString())
+                .hasDoc(params.get("hasDoc").toString())
+                .jobStatus(params.get("jobStatus").toString())
+                .deliveryStatus(params.get("deliveryStatus").toString())
+                .owner(params.get("owner").toString())
+                .remark(params.get("remark").toString())
+                .updateDate(new Date());
+        baseMapper.update(builder.build(), new QueryWrapper<IssueSummary>().lambda().eq(IssueSummary::getId, tempId));
     }
 }
