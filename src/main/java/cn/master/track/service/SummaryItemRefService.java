530a80a93f87f90e3b5f56f5240862a3fc0e5b15
jingll.li <the2ndindec@hotmail.com>
2021-08-10 16:14:13 +0800

    :art: 加注释

--- src/main/java/cn/master/track/service/SummaryItemRefService.java
+++ src/main/java/cn/master/track/service/SummaryItemRefService.java
@@ -7,13 +7,18 @@ import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author 11's papa
  * @since 2021-08-04
  */
 public interface SummaryItemRefService extends IService<SummaryItemRef> {
-
+    /**
+     * 保存关系
+     *
+     * @param item    问题单
+     * @param summary 问题汇总
+     */
     void addReference(IssueItem item, IssueSummary summary);
 }
