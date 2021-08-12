4344c4fa1515653fba5e333ae589a0249c3228a7
jingll.li <the2ndindec@hotmail.com>
2021-08-12 17:08:32 +0800

    :zap: 更新变量

--- src/main/java/cn/master/track/config/Constants.java
+++ src/main/java/cn/master/track/config/Constants.java
@@ -15,20 +15,8 @@ public class Constants {
 
     public static Map<String, List<TypeItem>> allTypes = new LinkedHashMap<>();
 
-    public static final List<String> MONTH_LIST = new LinkedList<String>() {{
-        add("2021-01");
-        add("2021-02");
-        add("2021-03");
-        add("2021-04");
-        add("2021-05");
-        add("2021-06");
-        add("2021-07");
-        add("2021-08");
-        add("2021-09");
-        add("2021-10");
-        add("2021-11");
-        add("2021-12");
-    }};
+    public static List<String> MONTH_LIST = new LinkedList<>();
+    public static List<String> PROJECT_ID_LIST = new LinkedList<>();
 
     public static final String SEVERITY_LEVEL = "severity_level";
     public static final String OWNER_LIST = "owner_list";
