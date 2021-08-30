018cde62d3aa25f929fa0470c34ef89668ee4fa5
jingll.li <the2ndindec@hotmail.com>
2021-08-30 15:38:39 +0800

    :zap: AllArgsConstructor NoArgsConstructor注解

--- src/main/java/cn/master/track/entity/IssueProject.java
+++ src/main/java/cn/master/track/entity/IssueProject.java
@@ -4,9 +4,7 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import lombok.Builder;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.*;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -23,6 +21,8 @@ import java.util.Date;
 @EqualsAndHashCode(callSuper = false)
 @TableName("issue_project")
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class IssueProject implements Serializable {
 
     private static final long serialVersionUID = 1L;
