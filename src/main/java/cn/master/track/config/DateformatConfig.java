4a6feb5a6b46cf312d9f13ecf19542f6fe0c9e64
jingll.li <the2ndindec@hotmail.com>
2021-09-15 15:25:23 +0800

    :zap: DateformatConfig

---
+++ src/main/java/cn/master/track/config/DateformatConfig.java
@@ -0,0 +1,48 @@
+package cn.master.track.config;
+
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Dateformat Configuration
+ *
+ * @author Created by 11's papa on 2021/09/13
+ * @version 1.0.0
+ */
+public class DateformatConfig {
+    /**
+     * Date格式化字符串
+     */
+    private static final String DATE_FORMAT = "yyyy-MM-dd";
+    /**
+     * DateTime格式化字符串
+     */
+    private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    /**
+     * Time格式化字符串
+     */
+    private static final String TIME_FORMAT = "HH:mm:ss";
+
+    @Bean
+    @Primary
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> builder.serializerByType(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)))
+                .serializerByType(LocalDate.class, new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .serializerByType(LocalTime.class, new LocalTimeSerializer(DateTimeFormatter.ofPattern(TIME_FORMAT)))
+                .deserializerByType(LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)))
+                .deserializerByType(LocalDate.class, new LocalDateDeserializer(DateTimeFormatter.ofPattern(DATE_FORMAT)))
+                .deserializerByType(LocalTime.class, new LocalTimeDeserializer(DateTimeFormatter.ofPattern(TIME_FORMAT)));
+    }
+}
